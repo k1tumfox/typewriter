ceae633ed58ff832c07444515887c4e9b867acbe
Tenyson Kwon <67251061+k1tumfox@users.noreply.github.com>
2020-08-06 10:38:42 -0400

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,19 +1,16 @@
-
-
 const sentence = "hello there from lighthouse labs";
 //console.log('12th index is: ', sentence.indexOf('f'));
 let timeDelay = 50;
 for (const char of sentence) {
   //process.stdout.write(timeDelay);
   
-  process.stdout.write(char);
   setTimeout(() => {
-
-  }, timeDelay);
+    process.stdout.write(char);
+    
+  }, timeDelay += 100);
   
-  timeDelay += 50;
-  
+    
 }
-process.stdout.write('\n');
-
-
+setTimeout(() => {
+  process.stdout.write('\n');
+}, 4000);
